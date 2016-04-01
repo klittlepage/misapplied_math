59283f1660ff12ef36df3f2fd44af9d4bbf26024
Kelly Littlepage <kelly.littlepage@gmail.com>
2016-04-01 13:40:50 -0500

    Updates to use the latest vendor assets
    Bootstrap version bump

--- _assets/javascripts/misapplied_site.js
+++ _assets/javascripts/misapplied_site.js
@@ -1,6 +1,6 @@
-//= require libs/jquery.min
-//= require libs/bootstrap.min
-//= require libs/modernizr-2.0
+//= require libs/jquery-1.12.2.min
+//= require libs/bootstrap-sprockets
+//= require libs/modernizr-3.3
 //= require libs/d3.v3.min
 //= require libs/holder.min
 //= require octopress
