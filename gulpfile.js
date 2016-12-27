7aa0e45bd33edfdb8a23de9515a5713be1d07069
Kelly Littlepage <kelly@onechronos.com>
2016-12-27 11:08:42 -0700

    Refactored to accomodate slight differences between the critical path css for site pages and blog pages.

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ var gutil = require('gulp-util');
 var critical = require('critical').stream;
 
 gulp.task('critical', function () {
-    return gulp.src(['build/index.html'],
-    	            { base: 'build/'})
+    return gulp.src(['build/index.html',
+                     'build/2014/01/08/testing-collections-with-guava-' +
+                     'testlib-and-junit-4/index.html'], { base: 'build/'})
         .pipe(critical({base: 'build/',
-                        dimensions: [{height: 900, width: 1300},
+                        dimensions: [{height: 1200, width: 1920},
+                                     {height: 900, width: 1300},
                                      {height: 600, width: 480}],
                         include: [/\.thumbnail.*/, /\.caption.*/],
                         minify: true}))
